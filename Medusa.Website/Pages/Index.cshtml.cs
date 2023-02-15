0ff3ccdcf17d7a45b6a04410fb8d69edfa239113
McThuggets <dylan.reuben1509@gmail.com>
2023-02-15 14:50:13 +0200

    Add project files.

---
+++ Medusa.Website/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Medusa.Website.Services;
+using Medusa.Website.Models;
+
+namespace Medusa.Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductsService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(
+                            ILogger<IndexModel> logger,
+                            JsonFileProductsService productService
+                          )
+        {
+            _logger = logger;
+            ProductService= productService;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
---
+++ Medusa.Website/Services/JsonFileProductsService.cs
@@ -0,0 +1,39 @@
+﻿using Medusa.Website.Models;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Medusa.Website.Services
+{
+    public class JsonFileProductsService
+    {
+
+        public JsonFileProductsService( IWebHostEnvironment webHostEnvironment) 
+        { 
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath,"data","products.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+
+
+    }
+}
